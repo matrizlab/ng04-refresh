b26b5a358ce5081236e8e2b2620aa5b86d4f8b16
matrizlab <info@matrizlab.com>
2019-04-23 19:39:28 +0200

    template bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products.component';
 import { ProductComponent } from './product/product.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductsComponent,
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
